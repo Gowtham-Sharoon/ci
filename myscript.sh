a03f199272edd45894e0dbf11de4953d3b7124a1
Ubuntu <ubuntu@ip-172-31-32-117.ap-south-1.compute.internal>
2024-02-19 03:33:23 +0000

    Final testing

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo "Hello, this is my script!"
+echo "File from ec2, executed on Jenkings by using Jenkins"
 echo "Current date and time: $(date)"
 
-echo "from ec2 now sending this file to run on jenkins"
